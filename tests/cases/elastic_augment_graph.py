5bd4b93e775b00b42ef7c7b8dc00d514a4107b90
pattonw <pattonw@hhmi.org>
2019-08-26 15:29:10 -0400

    add synthetic data generator

---
+++ tests/cases/elastic_augment_graph.py
@@ -0,0 +1,299 @@
+from gunpowder import (
+    ArrayKeys,
+    ArrayKey,
+    ArraySpec,
+    Array,
+    PointsKeys,
+    PointsKey,
+    PointsSpec,
+    GraphPoints as Points,
+    GraphPoint as Point,
+    BatchProvider,
+    BatchRequest,
+    Batch,
+    Roi,
+    Coordinate,
+    ElasticAugment,
+    RasterizePoints,
+    RasterizationSettings,
+    Snapshot,
+    build,
+)
+from .provider_test import ProviderTest
+
+import numpy as np
+import math
+import time
+import unittest
+
+
+class PointTestSource3D(BatchProvider):
+    def setup(self):
+
+        self.points = {
+            0: Point([0, 0, 0]),
+            1: Point([0, 10, 0]),
+            2: Point([0, 20, 0]),
+            3: Point([0, 30, 0]),
+            4: Point([0, 40, 0]),
+            5: Point([0, 50, 0]),
+        }
+
+        self.provides(
+            PointsKeys.TEST_POINTS,
+            PointsSpec(roi=Roi((-100, -100, -100), (200, 200, 200))),
+        )
+
+        self.provides(
+            ArrayKeys.TEST_LABELS,
+            ArraySpec(
+                roi=Roi((-100, -100, -100), (200, 200, 200)),
+                voxel_size=Coordinate((4, 1, 1)),
+                interpolatable=False,
+            ),
+        )
+
+    def point_to_voxel(self, array_roi, location):
+
+        # location is in world units, get it into voxels
+        location = location / self.spec[ArrayKeys.TEST_LABELS].voxel_size
+
+        # shift location relative to beginning of array roi
+        location -= array_roi.get_begin() / self.spec[ArrayKeys.TEST_LABELS].voxel_size
+
+        return tuple(slice(int(l - 2), int(l + 3)) for l in location)
+
+    def provide(self, request):
+
+        batch = Batch()
+
+        roi_points = request[PointsKeys.TEST_POINTS].roi
+        roi_array = request[ArrayKeys.TEST_LABELS].roi
+        roi_voxel = roi_array // self.spec[ArrayKeys.TEST_LABELS].voxel_size
+
+        data = np.zeros(roi_voxel.get_shape(), dtype=np.uint32)
+        data[:, ::2] = 100
+
+        for i, point in self.points.items():
+            loc = self.point_to_voxel(roi_array, point.location)
+            data[loc] = i
+
+        spec = self.spec[ArrayKeys.TEST_LABELS].copy()
+        spec.roi = roi_array
+        batch.arrays[ArrayKeys.TEST_LABELS] = Array(data, spec=spec)
+
+        points = {}
+        for i, point in self.points.items():
+            if roi_points.contains(point.location):
+                points[i] = point
+        batch.points[PointsKeys.TEST_POINTS] = Points(
+            points, PointsSpec(roi=roi_points)
+        )
+
+        return batch
+
+
+class TestElasticAugmentGraph(ProviderTest):
+    def test_3d_basics(self):
+
+        test_labels = ArrayKey("TEST_LABELS")
+        test_points = PointsKey("TEST_POINTS")
+        test_raster = ArrayKey("TEST_RASTER")
+
+        pipeline = (
+            PointTestSource3D()
+            + ElasticAugment(
+                [10, 10, 10],
+                [0.1, 0.1, 0.1],
+                # [0, 0, 0], # no jitter
+                [0, 2.0 * math.pi],
+            )
+            +  # rotate randomly
+            # [math.pi/4, math.pi/4]) + # rotate by 45 deg
+            # [0, 0]) + # no rotation
+            RasterizePoints(
+                test_points,
+                test_raster,
+                settings=RasterizationSettings(radius=2, mode="peak"),
+            )
+            + Snapshot(
+                {test_labels: "volumes/labels", test_raster: "volumes/raster"},
+                dataset_dtypes={test_raster: np.float32},
+                output_dir=self.path_to(),
+                output_filename="elastic_augment_test{id}-{iteration}.hdf",
+            )
+        )
+
+        for _ in range(5):
+
+            with build(pipeline):
+
+                request_roi = Roi((-20, -20, -20), (40, 40, 40))
+
+                request = BatchRequest()
+                request[test_labels] = ArraySpec(roi=request_roi)
+                request[test_points] = PointsSpec(roi=request_roi)
+                request[test_raster] = ArraySpec(roi=request_roi)
+
+                batch = pipeline.request_batch(request)
+                labels = batch[test_labels]
+                points = batch[test_points]
+
+                # the point at (0, 0, 0) should not have moved
+                self.assertTrue(0 in points.data, msg="{}".format(points.data))
+
+                labels_data_roi = (
+                    labels.spec.roi - labels.spec.roi.get_begin()
+                ) / labels.spec.voxel_size
+
+                # points should have moved together with the voxels
+                for i, point in points.data.items():
+                    loc = point.location - labels.spec.roi.get_begin()
+                    loc = Coordinate(loc / labels.spec.voxel_size)
+                    if labels_data_roi.contains(loc):
+                        self.assertEqual(
+                            labels.data[loc], i, msg="{}".format(points.data)
+                        )
+
+    @unittest.skipIf(
+        not hasattr(BatchRequest, "random_seed"), "random seed feature not installed!"
+    )
+    def test_random_seed(self):
+        test_labels = ArrayKey("TEST_LABELS")
+        test_points = PointsKey("TEST_POINTS")
+        test_raster = ArrayKey("TEST_RASTER")
+
+        pipeline = (
+            PointTestSource3D()
+            + ElasticAugment(
+                [10, 10, 10],
+                [0.1, 0.1, 0.1],
+                # [0, 0, 0], # no jitter
+                [0, 2.0 * math.pi],
+            )
+            +  # rotate randomly
+            # [math.pi/4, math.pi/4]) + # rotate by 45 deg
+            # [0, 0]) + # no rotation
+            RasterizePoints(
+                test_points,
+                test_raster,
+                settings=RasterizationSettings(radius=2, mode="peak"),
+            )
+            + Snapshot(
+                {test_labels: "volumes/labels", test_raster: "volumes/raster"},
+                dataset_dtypes={test_raster: np.float32},
+                output_dir=self.path_to(),
+                output_filename="elastic_augment_test{id}-{iteration}.hdf",
+            )
+        )
+
+        batch_points = []
+        for _ in range(5):
+
+            with build(pipeline):
+
+                request_roi = Roi((-20, -20, -20), (40, 40, 40))
+                request = BatchRequest(random_seed=10)
+                request[test_labels] = ArraySpec(roi=request_roi)
+                request[test_points] = PointsSpec(roi=request_roi)
+                request[test_raster] = ArraySpec(roi=request_roi)
+
+                batch = pipeline.request_batch(request)
+                labels = batch[test_labels]
+                points = batch[test_points]
+                batch_points.append(
+                    tuple(
+                        (key, tuple(point.location))
+                        for key, point in points.data.items()
+                    )
+                )
+
+                # the point at (0, 0, 0) should not have moved
+                self.assertTrue(0 in points.data)
+
+                labels_data_roi = (
+                    labels.spec.roi - labels.spec.roi.get_begin()
+                ) / labels.spec.voxel_size
+
+                # points should have moved together with the voxels
+                for i, point in points.data.items():
+                    loc = point.location - labels.spec.roi.get_begin()
+                    loc = loc / labels.spec.voxel_size
+                    loc = Coordinate(int(round(x)) for x in loc)
+                    if labels_data_roi.contains(loc):
+                        self.assertEqual(labels.data[loc], i)
+
+        for point_data in zip(*batch_points):
+            self.assertEqual(len(set(point_data)), 1)
+
+    @unittest.skipIf(
+        not hasattr(BatchRequest, "random_seed"), "random seed feature not installed!"
+    )
+    def test_cache(self):
+
+        test_labels = ArrayKey("TEST_LABELS")
+        test_points = PointsKey("TEST_POINTS")
+        test_raster = ArrayKey("TEST_RASTER")
+
+        pipeline = (
+            PointTestSource3D()
+            + ElasticAugment(
+                [10, 10, 10],
+                [0.1, 0.1, 0.1],
+                # [0, 0, 0], # no jitter
+                [0, 2.0 * math.pi],
+            )
+            +  # rotate randomly
+            # [math.pi/4, math.pi/4]) + # rotate by 45 deg
+            # [0, 0]) + # no rotation
+            RasterizePoints(
+                test_points,
+                test_raster,
+                settings=RasterizationSettings(radius=2, mode="peak"),
+            )
+            + Snapshot(
+                {test_labels: "volumes/labels", test_raster: "volumes/raster"},
+                dataset_dtypes={test_raster: np.float32},
+                output_dir=self.path_to(),
+                output_filename="elastic_augment_test{id}-{iteration}.hdf",
+            )
+        )
+
+        times = {0: [], 1: []}
+        for seed in range(10):
+            for i in range(2):
+
+                with build(pipeline):
+
+                    request_roi = Roi((-40, -40, -40), (80, 80, 80))
+
+                    request = BatchRequest(random_seed=seed)
+                    request[test_labels] = ArraySpec(roi=request_roi)
+                    request[test_points] = PointsSpec(roi=request_roi)
+                    request[test_raster] = ArraySpec(roi=request_roi)
+
+                    t1 = time.time()
+                    batch = pipeline.request_batch(request)
+                    labels = batch[test_labels]
+                    points = batch[test_points]
+                    t2 = time.time()
+                    times[i].append(t2 - t1)
+
+                    # the point at (0, 0, 0) should not have moved
+                    self.assertTrue(0 in points.data)
+
+                    labels_data_roi = (
+                        labels.spec.roi - labels.spec.roi.get_begin()
+                    ) / labels.spec.voxel_size
+
+                    # points should have moved together with the voxels
+                    for i, point in points.data.items():
+                        loc = point.location - labels.spec.roi.get_begin()
+                        loc = loc / labels.spec.voxel_size
+                        loc = Coordinate(int(round(x)) for x in loc)
+                        if labels_data_roi.contains(loc):
+                            self.assertEqual(labels.data[loc], i)
+
+        # the second request should be much faster since the transform has been cached
+        self.assertLess(np.mean(times[1]), np.mean(times[0]) / 5)
+
